ea7d2dc5c1f77d0659b40cc93c7ddbb5e3553169
MaPole <ysh.skyblue6485@gmail.com>
2024-10-01 21:14:47 +0900

    [스프링 입문] 03 회원 관리 예제 - 백엔드 개발

---
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        // store에서 값(member)들을 불러와 각 값들의 name과 매개변수 name를 비교
+        // 하나라도 같은 값이 있다면 그 name을 가진 member를 Optional로 감싸서 출력
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
